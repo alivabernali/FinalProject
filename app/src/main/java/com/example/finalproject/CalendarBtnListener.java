c55eab70d21669b10e31dc6e0c75fde30460fb07
aasthapatel <aasthap2@illinois.edu>
2019-12-08 13:17:45 -0600

    snackbar

--- app/src/main/java/com/example/finalproject/CalendarBtnListener.java
+++ app/src/main/java/com/example/finalproject/CalendarBtnListener.java
@@ -1,12 +1,23 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.view.View;
+
+import java.util.Calendar;
 
 public class CalendarBtnListener implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
         // Code to add to calendar
-
+        Calendar calendarEvent = Calendar.getInstance();
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra("beginTime", calendarEvent.getTimeInMillis());
+        intent.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000);
+        intent.putExtra("title", "Sample Event");
+        intent.putExtra("allDay", true);
+        intent.putExtra("rule", "FREQ=YEARLY");
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
